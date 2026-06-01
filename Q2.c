1bbcdb914ee1e73aeaf9f07acd996e83c8c65551
AlmaipetShashank <shashank.almaipet17@gmail.com>
2026-06-01 06:35:27 +0000

    changes made

---
+++ Q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float radius;
+    printf("enter radius");
+    scanf("%f", &radius);
+
+    printf("area of circle is %f \n", 3.14f * radius * radius);
+    return 0;
+}
