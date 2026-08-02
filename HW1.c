56c44340e88a11cd66dfdb6517643b3eaa24cb1e
AlmaipetShashank <shashank.almaipet17@gmail.com>
2026-08-02 21:21:25 +0530

    Rectangle Perimeter calculation in HW1.c

---
+++ HW1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( ) {
+    int a, b;
+
+    printf("Enter a value : ");
+    scanf("%d", &a);
+    printf("Enter b value : ");
+    scanf("%d", &b);
+
+    printf("This is the Perimeter of Rectangle %d", 2 * a * b);
+    return 0;
+}
